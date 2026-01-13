25570e5aff896e75f1b0ab3153c0a1d8e31e66ab
eklundkristoffer <eklukris@gmail.com>
2026-01-13 20:58:16 +0100

    WIP

--- src/Column.php
+++ src/Column.php
@@ -6,19 +6,10 @@ namespace Eklundlabs\InertiaDatatable;
 
 use Eklundlabs\InertiaDatatable\Contracts\ColumnInterface;
 use Illuminate\Contracts\Support\Arrayable;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Str;
 
-/**
- * @property null|string $url
- */
 abstract class Column implements Arrayable, ColumnInterface
 {
     public bool $searchable = false;
-
-    public array $attributes = [];
-
-    public mixed $value;
 
     public $link;
 
@@ -27,13 +18,6 @@ abstract class Column implements Arrayable, ColumnInterface
     public array $mappings = [];
 
     public array $iconMappings = [];
-
-    public function toDatatableRow(Model $row): array
-    {
-        return [
-            'value' => $row->getAttribute($this->name()),
-        ];
-    }
 
     public function __construct(public string $column, public string $label) {}
 
@@ -54,44 +38,6 @@ abstract class Column implements Arrayable, ColumnInterface
         return $this->column;
     }
 
-    public function value(mixed $value): mixed
-    {
-        return $value;
-    }
-
-    public function setAttribute(string $key, mixed $value): static
-    {
-        $this->attributes[$key] = $value;
-
-        return $this;
-    }
-
-    public function attributes()
-    {
-        return $this->attributes;
-    }
-
-    public function hasAbility(string $ability)
-    {
-        $abilities = collect($this->abilities)
-            ->filter(fn ($a) => Str::afterLast($a, '\\') == $ability);
-
-        dd($abilities->sole());
-
-        dd(array_filter($this->abilities, fn ($a) => Str::afterLast($a, '\\') == $ability));
-
-        return in_array($ability, class_uses($this), true);
-    }
-
-    public function applyMappings(): void
-    {
-        $mappings = $this->mappings;
-
-        if (count($mappings) && isset($mappings[$this->value])) {
-            $this->value = $mappings[$this->value];
-        }
-    }
-
     public function map(array $mappings): static
     {
         $this->mappings = $mappings;
@@ -106,45 +52,12 @@ abstract class Column implements Arrayable, ColumnInterface
         return $this;
     }
 
-    public function __call(string $name, array $arguments): mixed
-    {
-        $abilityName = str($name)
-            ->append('Ability')
-            ->pascal()
-            ->toString();
-
-        dd($this->hasAbility($abilityName));
-
-        dd(str($name)->append('Ability')->pascal(), $arguments);
-    }
-
-    public function forRow(Model $row): array
-    {
-        $rawValue = $row->getAttribute($this->name());
-
-        $this->value = $rawValue;
-
-        foreach ($this->abilities as $ability) {
-            $ability = app()->make($ability);
-
-            dd($ability);
-        }
-
-        $this->applyMappings();
-
-        return [
-            'value' => $this->value,
-            'icon' => 'badge-check',
-        ];
-    }
-
     public function toArray(): array
     {
         return [
             'column' => $this->column,
             'label' => $this->label,
             'type' => class_basename(static::class),
-            // "attributes" => $this->attributes(),
         ];
     }
 }
--- src/Contracts/ColumnInterface.php
+++ src/Contracts/ColumnInterface.php
@@ -5,6 +5,4 @@ namespace Eklundlabs\InertiaDatatable\Contracts;
 interface ColumnInterface
 {
     public function __construct(string $column, string $label);
-
-    public function value(mixed $value): mixed;
 }
